592e1abffc9d01e12c91d2bf5cec299bed34e194
Denis Karmalak <vealo@yandex.ru>
2024-07-24 16:21:53 +0300

    Add app hr

--- config/settings.py
+++ config/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     #APP pip
-    'typing_extensions'
+    'django_extensions',
 
     #APP progect
     'hr'
--- hr/models.py
+++ hr/models.py
@@ -1,3 +1,14 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+class Employee(models.Model):
+    class Workplace(models.IntegerChoices):
+        JOB_1 = 1, 'Junior'
+        JOB_2 = 2, 'Middle'
+        JOB_3 = 3, 'Senior'
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    workspace = models.PositiveSmallIntegerField(choices=Workplace.choices, default=Workplace.JOB_1,
+                                                 help_text="Position in the company?")
