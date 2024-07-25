b6bb9473d29a5149d26a8b736294842ecc81c661
Denis Karmalak <vealo@yandex.ru>
2024-07-25 11:09:33 +0300

    Add class: Compensation, Contact, Department

--- hr/admin.py
+++ hr/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import Contact, Employee, Department
 # Register your models here.
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ['phone', 'adress']
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'workspace', 'contact', 'department']
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description']
--- hr/models.py
+++ hr/models.py
@@ -1,6 +1,26 @@
 import uuid
 from django.db import models
 
+class Contact(models.Model):
+    phone = models.CharField(max_length=50, unique=True)
+    adress = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.phone
+
+class Department(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Compensation(models.Model):
+    name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
+
 class Employee(models.Model):
     class Workplace(models.IntegerChoices):
         JOB_1 = 1, 'Junior'
@@ -10,5 +30,12 @@ class Employee(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    workspace = models.PositiveSmallIntegerField(choices=Workplace.choices, default=Workplace.JOB_1,
-                                                 help_text="Position in the company?")
+    workspace = models.PositiveSmallIntegerField(choices=Workplace.choices, default=Workplace.JOB_1, help_text="Position in the company?")
+    contact = models.OneToOneField(Contact, on_delete=models.CASCADE, null=True)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE, null=True, default=None)
+    compensation = models.ManyToManyField(Compensation)
+
+    def __str__(self):
+        return f'{self.first_name}, {self.last_name}'
+
+
